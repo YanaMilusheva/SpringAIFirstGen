c2e961bcda322c2ce415026a4ecb55a1b4ea93fe
Yana Milusheva <80757913+YanaMilusheva@users.noreply.github.com>
2024-09-07 15:23:46 +0300

    Initial commit

---
+++ src/main/java/com/ymi/firstgen/SpringAIFirstGen.java
@@ -0,0 +1,13 @@
+package com.ymi.firstgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIFirstGen {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAIFirstGen.class, args);
+    }
+
+}
